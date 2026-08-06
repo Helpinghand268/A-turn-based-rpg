799ef45151fbb8058d4eb79d57f6ab2513103ec8
Helpinghand268 <138344757+Helpinghand268@users.noreply.github.com>
2026-08-06 15:53:56 -0700

    Functions.h

--- Functions.h
+++ Functions.h
@@ -4,14 +4,22 @@
 class PlayerMenu
 {
     private:
-        const int heal = 1;
-        const int fire_ball = 2;
-        const int blizzard = 4;
-        const int lightning_bolt = 8;
+        const int heal = 1;             //cost 1 mana
+        const int fire_ball = 2;        //Unlocked at level 3, cost 3 mana
+        const int blizzard = 4;         //Unlocked at level 5, cost 5 mana
+        const int lightning_bolt = 8;   //Unlocked at level 7, cost 7 mana
         
-        int health = 100;
-        int mana_pool = 5;
-        int current_skills = 1;
+        const int attack = 1;           //cost no mana, scales damage each level
+        const int charged_attack = 2;   //Unlocked at level 2, cost 1 mana
+        const int flurry_rush = 4;      //Unlocked at level 4, cost 2 mana
+        const int blade_tornado = 8;    //Unlocked at level 6, cost 3 mana, leaves player tired for a few turns
+
+        int health = 100;               //Level 2: 120, level 3: 140, level 4: 170, level 5: 190, level 6: 210, level 7: 240
+        int mana_pool = 5;              //Level 2: 5, level 3: 6, level 4: 6, level 5: 7, level 6: 7, level 7: 8
+        
+        int current_spells = 1;
+        int attack_options = 1;
+
         int current_level = 1;
 
     public:
@@ -24,5 +32,7 @@ class PlayerMenu
     void combat();
     void closeGame();
 };
+
+
 
 #endif
